a87d2d0facdd826e048ab817517e6c8772582eed
Ilan Olkies <ilanolkies@gmail.com>
2025-08-29 17:04:10 -0300

    feat(alpen): use gh pages base path

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ if (process.env.NEXT_PUBLIC_ENABLE_STAKING === 'true') pageExtensions.push('stak
 
 /** @type {import('next').NextConfig} */
 module.exports = withBundleAnalyzer({
+  assetPrefix: '/chain-check',
+  basePath: '/chain-check',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
